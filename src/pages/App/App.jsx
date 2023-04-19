aa4ab888b531623591600eab2772e640db986b89
guantanamobosch <luketyson17@gmail.com>
2023-04-18 18:22:13 -0600

    file architecture - pages and components

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+    return (
+        <main className="App">
+            <h1>Luke Tyson's Portfolio</h1>
+        </main>
+    );
+}
+
+export default App;

ac3e962caa273a0a923dd1898ce631f5a4a5db0b
guantanamobosch <luketyson17@gmail.com>
2023-05-03 16:39:06 -0600

    write an about me

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import "./AboutMe.css";
 
 export default function AboutMe() {
-    return <div>AboutMe</div>;
+    return (
+        <div id="about-container">
+            <h1 id="about-header">About Me</h1>
+            <p>
+                Hello and welcome! My name is Luke Tyson, I am a fullstack
+                developer (but you already knew that from the nav bar), and this
+                is my portfolio website! I appreciate your taking the time to
+                visit and look at my work, so thank you!
+            </p>
+            <p>
+                I started my coding journey in 2023 through General Assembly's
+                12-week Software Engineering Immersive, and through their
+                program I acquired a skill-base centered around fullstack
+                web-development, utilizing technologies including but not
+                limited to HMTL, CSS, Javascript, Python, Django, Express, and
+                React.
+            </p>
+            <p>
+                Besides being a coder, I am also an avid reader, a prolific
+                musician, a butter-fingered climber of rocks, a Dungeons and
+                Dragons enjoyer, an amateur weightlifter, an ex-chimney sweep, a
+                local hero of slam poetry, and a lifelong folder of all things
+                laundry.
+            </p>
+            <p>
+                Please use the nav bar above to check out my resume and my
+                portfolio projects. If you are a recruiter, employer, fellow
+                programmer, or if you've got some laundry for me to fold, please
+                feel free to reach out to me through the contact information on
+                the left.
+            </p>
+        </div>
+    );
 }
